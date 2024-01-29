12dadf567df8f25c9e0f43620b7a4f5d92ed0135
Francis1a <52089780+Francis1a@users.noreply.github.com>
2024-01-29 18:00:16 +0800

    remove links

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -11,7 +11,7 @@ export const useSignup = () => {
     setError(null)
 
     //Signing up the user
-    const response = await fetch('https://workout-data.onrender.com/api/user/signup', {
+    const response = await fetch('/api/user/signup', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password })
